7e5d7093f6b1958c90dab9918a5dbc1722409afd
Adriel Vinicius <37392070+adriel1234@users.noreply.github.com>
2024-10-22 20:38:06 -0400

    update procts edit files

--- src/app/products/product-item/product-item.component.ts
+++ src/app/products/product-item/product-item.component.ts
@@ -1,18 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, Injector, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {Product} from '../../../shared/models/product';
+import {URLS} from '../../../shared/urls';
+
 
 @Component({
   selector: 'app-product-item',
-  standalone: true,
-  imports: [],
   templateUrl: './product-item.component.html',
   styleUrl: './product-item.component.css'
 })
-export class ProductItemComponent {
-  public data: Product;
-  constructor(private http: HttpClient) {
+export class ProductItemComponent  implements OnInit{
+  public data: Product = {} as Product;
+  constructor(private http: HttpClient, private injector:Injector) {
+    this.http = injector.get(HttpClient)
 
+  }
 
+  public ngOnInit() {
+    this.fetchData(1);
+  }
+
+  fetchData(id: number){
+    this.http.get<Product>(URLS.PRODUCT+id)
+      .subscribe({
+        next:(data:Product)=>{
+          this.data = data;
+          console.log(this.data);
+        },
+        error:(error:any)=>{
+          console.error('There wa an error!', error);
+        },
+        complete:()=>{
+          console.log('Completed!');
+        }
+      });
   }
 
 }
