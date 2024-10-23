687820ade5d6193795564a8ab2af4cb598bf0892
Adriel Vinicius <37392070+adriel1234@users.noreply.github.com>
2024-10-23 19:01:43 -0400

    update product

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import {MatButton} from '@angular/material/button';
+import {MatCard} from '@angular/material/card';
+import {
+  MatCellDef,
+  MatColumnDef,
+  MatHeaderCell,
+  MatHeaderCellDef,
+  MatHeaderRow,
+  MatTable, MatTableModule
+} from '@angular/material/table';
+import {Product} from '../../shared/models/product';
 
 @Component({
   selector: 'app-products',
+  standalone: true,
+  imports: [
+    MatTableModule,
+    MatCard,
+    MatButton
+  ],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
 export class ProductsComponent {
+  public dataSource: Product[] = [];
+  displayedColumns: string[] = ['description', 'quantity'];
 
 }
