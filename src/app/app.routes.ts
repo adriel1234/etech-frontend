274086c672fc67c46bcc0b95508215802f97ed72
Adriel Vinicius <adriel1234.1234@gmail.com>
2024-10-29 09:44:36 -0400

    edit app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import {ProductListComponent} from '../base/product-list/product-list.component';
 import {ClientListComponent} from '../base/client-list/client-list.component';
+import {EmployeeListComponent} from '../base/employee-list/employee-list.component';
+import {SaleListComponent} from '../base/sale-list/sale-list.component';
 
 export const routes: Routes = [
   {
@@ -10,5 +12,13 @@ export const routes: Routes = [
   {
     path: 'client',
     component: ClientListComponent,
+  },
+  {
+    path: 'employee',
+    component: EmployeeListComponent,
+  },
+  {
+    path: 'sale',
+    component: SaleListComponent,
   }
 ];
