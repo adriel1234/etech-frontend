34805bb07869b82c1bf7023d95cb3a3b064357bf
Adriel Vinicius <37392070+adriel1234@users.noreply.github.com>
2024-10-29 20:28:53 -0400

    sale-list service impl

--- src/base/sale-list/sale-list.component.ts
+++ src/base/sale-list/sale-list.component.ts
@@ -9,10 +9,11 @@ import {MatCard} from '@angular/material/card';
 import {MatTableModule} from '@angular/material/table';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormsModule} from '@angular/forms';
-import {MatButton, MatIconButton} from '@angular/material/button';
+import {MatButton, MatFabButton, MatIconButton} from '@angular/material/button';
 import {MatInput} from '@angular/material/input';
 import {MatIcon} from '@angular/material/icon';
 import {MatPaginator} from '@angular/material/paginator';
+import {BaseService} from '../../shared/services/base.service';
 
 @Component({
   selector: 'app-sale-list',
@@ -27,6 +28,7 @@ import {MatPaginator} from '@angular/material/paginator';
     MatIcon,
     MatPaginator,
     MatButton,
+    MatFabButton,
   ],
   templateUrl: './sale-list.component.html',
   styleUrl: './sale-list.component.css'
@@ -39,20 +41,24 @@ export class SaleListComponent  implements OnInit{
 
   // @ViewChild(MatPaginator, {static:true}) paginator: MatPaginator;
 
-  private parameters: HttpParams = new HttpParams();
+
+  private service: BaseService<Sale>;
 
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.service = new BaseService<Sale>(http,URLS.SALE);
+  }
+
 
   public ngOnInit(): void {
     this.search();
   }
 
   public search(resetIndex: boolean = false): void {
-    this.clearParameters();
-    this.addParameter('nrf',this.searchNRF);
-    this.addParameter('employee__id', this.searchValue);
-    this.getAll<Sale>(URLS.SALE).subscribe({
+    this.service.clearParameter();
+    this.service.addParameter('nrf',this.searchNRF);
+    this.service.addParameter('employee__id', this.searchValue);
+    this.service.getAll().subscribe({
       next: (data: Sale[]) => {
         this.dataSource = data;
       },
@@ -62,56 +68,14 @@ export class SaleListComponent  implements OnInit{
     });
   }
 
-  public deleteObject(id: number):void{
-    this.delete(id,URLS.SALE).subscribe({
-      next:(_:any):void =>{
-        this.search();
-      },
-      error: (_:any):void =>{
-        console.error('Error delete sales');
-      }
-    })
-  }
-  //service
-
-  public getAll<T>(route: string): Observable<T[]> {
-    const url = URLS.BASE + route;
-    return this.http.get<T[]>(url,this.getOptions());
-  }
-
-  public delete(id:number | string, route:string):Observable<any>{
-    this.clearParameters();
-    const url = URLS.BASE + route + id;
-
-    return this.http.delete<any>(url,this.getOptions());
-  }
-
-  public save<T>(entity: T, route:string): Observable<T> {
-    this.clearParameters();
-    const url = URLS.BASE + route;
-    return this.http.post<T>(url,entity,this.getOptions()) as Observable<T>;
-  }
-
-  public update<T>(id:number | string,entity: any): Observable<T> {
-    this.clearParameters();
-    const url = URLS.BASE+id+'/';
-    return this.http.patch<T>(url,entity,this.getOptions()) as Observable<T>;
-  }
-
-  public addParameter(key: string, value: string): void {
-    this.parameters = this.parameters.set(key, value);
-  }
-
-  public clearParameters(): void {
-    this.parameters = new HttpParams();
-  }
-
-  public getOptions(): HttpOptions {
-    const httpOptions: HttpOptions = {}
-    if(this.parameters){
-      httpOptions.params = this.parameters;
-    }
-    return  httpOptions;
-  }
-
+  // public deleteObject(id: number):void{
+  //   this.delete(id,URLS.SALE).subscribe({
+  //     next:(_:any):void =>{
+  //       this.search();
+  //     },
+  //     error: (_:any):void =>{
+  //       console.error('Error delete sales');
+  //     }
+  //   })
+  // }
 }
