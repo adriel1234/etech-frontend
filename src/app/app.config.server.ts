da6a587e3ae4dd86b083ff419b3fe13f8f3548ce
Adriel Vinicius <37392070+adriel1234@users.noreply.github.com>
2024-10-30 19:33:13 -0400

    update appcomponet e style css

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,11 +1,49 @@
-import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
-import { provideServerRendering } from '@angular/platform-server';
-import { appConfig } from './app.config';
+import { Component } from '@angular/core';
+import {NavigationExtras, Router, RouterOutlet} from '@angular/router';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButton, MatFabButton} from "@angular/material/button";
+import {MatIcon} from "@angular/material/icon";
+import {NgClass} from '@angular/common';
 
-const serverConfig: ApplicationConfig = {
-  providers: [
-    provideServerRendering()
-  ]
-};
+interface Menu{
+  title: string;
+  route: string;
+  isCurrent: boolean;
+}
 
-export const config = mergeApplicationConfig(appConfig, serverConfig);
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, MatFabButton, MatIcon, MatButton, NgClass],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  public menuList: Menu[];
+
+  private router: Router = new Router();
+
+  constructor() {
+    this.menuList = [
+      {title: 'PRODUTOS', route: '/product', isCurrent: false},
+      {title: 'CLIENTES', route: '/client', isCurrent: false},
+      {title: 'FUNCIONÁRIO', route: '/employee', isCurrent: false},
+      {title: 'VENDAS', route: '/sale', isCurrent: false},
+    ];
+    this.changeMenu(this.menuList[0]);
+  }
+
+  public changeMenu(menu: Menu): void {
+    this.menuList.forEach((m: Menu) => {
+      m.isCurrent = m.route === menu.route;
+    });
+    this.goToPage(menu.route);
+  }
+
+  public goToPage(route: string): void {
+    const extras: NavigationExtras = {queryParamsHandling: 'merge'};
+    this.router.navigate([route], extras).then();
+  }
+
+}
