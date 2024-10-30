52b89f8167623942d078ff8c755f87b245428aee
Adriel Vinicius <37392070+adriel1234@users.noreply.github.com>
2024-10-29 20:26:39 -0400

    product-list service impl

--- src/base/product-list/product-list.component.ts
+++ src/base/product-list/product-list.component.ts
@@ -1,8 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient, HttpParams} from '@angular/common/http';
-import { Product } from '../../shared/models/product';
+import {Product} from '../../shared/models/product';
 import {elementAt, Observable} from 'rxjs';
-import { URLS } from '../../shared/urls';
+import {URLS} from '../../shared/urls';
 import {MatCard} from '@angular/material/card';
 import {MatTableModule} from '@angular/material/table';
 import {MatFormFieldModule} from '@angular/material/form-field';
@@ -13,6 +13,7 @@ import {MatIcon} from '@angular/material/icon';
 import {HttpOptions} from '../../shared/http/http-options';
 import {MatPaginator} from '@angular/material/paginator';
 import * as url from 'node:url';
+import {BaseService} from '../../shared/services/base.service';
 
 @Component({
   selector: 'app-product-list',
@@ -33,26 +34,30 @@ import * as url from 'node:url';
 })
 export class ProductListComponent implements OnInit {
   public dataSource: Product[] = [];
-  public displayedColumns:string[] = ['id', 'description', 'quantity','actions'];
-  public searchValue:string = '';
+  public displayedColumns: string[] = ['id', 'description', 'quantity', 'actions'];
+  public searchValue: string = '';
   public searchQtd: string = '';
 
   // @ViewChild(MatPaginator, {static:true}) paginator: MatPaginator;
 
-  private parameters: HttpParams = new HttpParams();
+  // private router:Router = new Router();
+
+  private service: BaseService<Product>;
 
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.service = new BaseService<Product>(http,URLS.PRODUCT);
+  }
 
   public ngOnInit(): void {
     this.search();
   }
 
   public search(resetIndex: boolean = false): void {
-    this.clearParameters();
-    this.addParameter('quantity_gt',this.searchQtd);
-    this.addParameter('description', this.searchValue);
-    this.getAll<Product>(URLS.PRODUCT).subscribe({
+    this.service.clearParameter();
+    this.service.addParameter('quantity_gt', this.searchQtd);
+    this.service.addParameter('description', this.searchValue);
+    this.service.getAll().subscribe({
       next: (data: Product[]) => {
         this.dataSource = data;
       },
@@ -62,58 +67,4 @@ export class ProductListComponent implements OnInit {
     });
   }
 
-public deleteObject(id: number):void{
-    this.delete(id,URLS.PRODUCT).subscribe({
-      next:(_:any):void =>{
-        this.search();
-      },
-      error: (_:any):void =>{
-        console.error('Error delete products');
-      }
-    })
-}
-  //service
-
-  public getAll<T>(route: string): Observable<T[]> {
-    const url = URLS.BASE + route;
-    return this.http.get<T[]>(url,this.getOptions());
-  }
-
-  public delete(id:number | string, route:string):Observable<any>{
-    this.clearParameters();
-    const url = URLS.BASE + route + id;
-
-    return this.http.delete<any>(url,this.getOptions());
-  }
-
- public save<T>(entity: T, route:string): Observable<T> {
-   this.clearParameters();
-   const url = URLS.BASE + route;
-   return this.http.post<T>(url,entity,this.getOptions()) as Observable<T>;
- }
-
-  public update<T>(id:number | string,entity: any): Observable<T> {
-    this.clearParameters();
-    const url = URLS.BASE+id+'/';
-    return this.http.patch<T>(url,entity,this.getOptions()) as Observable<T>;
-  }
-
-  public addParameter(key: string, value: string): void {
-    this.parameters = this.parameters.set(key, value);
-  }
-
-  public clearParameters(): void {
-    this.parameters = new HttpParams();
-  }
-
-  public getOptions(): HttpOptions {
-    const httpOptions: HttpOptions = {}
-    if(this.parameters){
-      httpOptions.params = this.parameters;
-    }
-    return  httpOptions;
-  }
-
-
-  protected readonly elementAt = elementAt;
 }
