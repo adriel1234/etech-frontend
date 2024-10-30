3f0c5602327464ef0b4027aee1dc8d09c73e641c
Adriel Vinicius <37392070+adriel1234@users.noreply.github.com>
2024-10-30 19:31:33 -0400

    update app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,18 @@
 import { Routes } from '@angular/router';
 import {ProductListComponent} from '../base/product-list/product-list.component';
 import {ClientListComponent} from '../base/client-list/client-list.component';
-import {EmployeeListComponent} from '../base/employee-list/employee-list.component';
 import {SaleListComponent} from '../base/sale-list/sale-list.component';
+import {EmployeeListComponent} from '../base/employee-list/employee-list.component';
+import {ProductItemComponent} from '../base/product-list/product-item/product-item.component';
 
 export const routes: Routes = [
   {
     path: 'product',
     component: ProductListComponent,
+  },
+  {
+    path: 'product/:action',
+    component: ProductItemComponent,
   },
   {
     path: 'client',
